5b85fecef25b2524714369f031cfc486f2219111
Simon Read <sread@smcm.edu>
2020-05-20 15:46:41 -0400

    Added initial interpreter for Genetic Programming.

--- Solitaire Project/src/edu/smcm/util/ImplementationErrorException.java
+++ Solitaire Project/src/edu/smcm/util/ImplementationErrorException.java
@@ -2,18 +2,46 @@ package edu.smcm.util;
 
 @SuppressWarnings("serial")
 public class ImplementationErrorException extends RuntimeException {
+	// TODO This should be a hierarchy
+	// TODO This should be called ImplementationError as it's a RuntimeException or should it be an Error?
 	
 	private Exception exception;
+	private String message;
+	
+	public ImplementationErrorException() {
+		this.exception = null;
+		this.message = null;
+	}
 	
 	public ImplementationErrorException(Exception exception) {
 		this.exception = exception;
+		this.message = null;
+	}
+	
+	public ImplementationErrorException(String message) {
+		this.exception = null;
+		this.message = message;
 	}
 	
 	public Exception exception() {
 		return exception;
 	}
 	
+	public String message() {
+		return message;
+	}
+	
 	public String toString() {
-		return "Implementation Error: " + exception;
+		String result;
+		
+		if (null != exception) { 
+			result = "Implementation Error: " + exception;
+		} else if (null != message) {
+			result = "Implementation Error: " + message;
+		} else {
+			result = "Unknown implementation error.";
+		}
+		
+		return result;
 	}
 }
